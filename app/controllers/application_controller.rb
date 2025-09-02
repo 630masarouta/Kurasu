8ce195a8fe8aeaab9922450d880743f1a0792237
630masarouta <gaoliuyaxing@gmail.com>
2025-09-02 23:18:40 +0900

    ログインログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth if Rails.env.production?
+  helper_method :current_user, :logged_in?
 
   private
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
